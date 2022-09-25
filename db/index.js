7cd613d0fd859057d2af3d6e9f8bf46dcd53be13
JackPadalino <pada0867@gmail.com>
2022-09-25 18:30:31 -0400

    seeded DB with Player and Game instances

--- db/index.js
+++ db/index.js
@@ -2,44 +2,26 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/roshambo');
 
 
+const Game = db.define('game',{
+    result:{
+        type:Sequelize.ENUM(['human','computer','tie']),
+        allowNull:false
+    }
+});
+
 const Player = db.define('player',{
     username:{
         type:Sequelize.STRING,
+        unique:true,
         allowNull:false
     },
-    being:{
-        type:Sequelize.STRING,
-        allNull:false
-    }
 });
 
-const Game = db.define('game',{
-    player1Id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: Player,
-          key: 'id'
-        }
-      },
-    player2Id: {
-        type: Sequelize.INTEGER,
-        references: {
-            model: Player,
-            key: 'id'
-        }
-    },
-    winner:{
-        type:Sequelize.STRING,
-        allowNull:false
-    }
-});
-
-Game.hasMany(Player);
-Player.belongsTo(Game);
+Player.hasMany(Game);
+Game.belongsTo(Player);
 
 module.exports = {
     db,
     Game,
-    Player,
-    PlayerGame
+    Player
 };
