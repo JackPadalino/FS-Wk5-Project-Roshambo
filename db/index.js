5d06c5143c6bf5241d870a484b0a172f5fd81d35
JackPadalino <pada0867@gmail.com>
2022-09-25 21:11:18 -0400

    updated DB

--- db/index.js
+++ db/index.js
@@ -5,7 +5,11 @@ const db = new Sequelize('postgres://localhost:5432/roshambo');
 const Game = db.define('game',{
     result:{
         type:Sequelize.ENUM(['human','computer','tie']),
-        allowNull:false
+        allowNull:false,
+        get(){
+            const result = this.getDataValue('result');
+            return result[0].toUpperCase() + result.slice(1);
+        }
     }
 });
 
